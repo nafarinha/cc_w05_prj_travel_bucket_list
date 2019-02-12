87fb019df493f5019c9670a58ae2f1b1b121bd55
Nuno Farinha <nadfarinha@gmail.com>
2019-02-12 11:11:29 +0000

    Create database tables and seed data

---
+++ db/seeds.rb
@@ -0,0 +1,96 @@
+require_relative '../models/country.rb'
+require_relative '../models/city.rb'
+require_relative '../models/destination.rb'
+require 'pry-byebug'
+
+Destination.delete_all()
+City.delete_all()
+Country.delete_all()
+
+country1 = Country.new({
+  "name" => "Spain",
+  "continent" => "Europe"
+})
+country2 = Country.new({
+  "name" => "France",
+  "continent" => "Europe"
+})
+country3 = Country.new({
+  "name" => "Germany",
+  "continent" => "Europe"
+})
+
+country1.save()
+country2.save()
+country3.save()
+
+city1 = City.new({
+  "name" => "Madrid",
+  "lat" => +40.383333,
+  "long" => -3.716667,
+  "country_id" => country1.id
+  })
+city2 = City.new({
+  "name" => "Seville",
+  "lat" => +37.39,
+  "long" => -5.99,
+  "country_id" => country1.id
+  })
+city3 = City.new({
+  "name" => "Valencia",
+  "lat" => +39.466667,
+  "long" => -0.375,
+  "country_id" => country1.id
+  })
+city4 = City.new({
+  "name" => "Paris",
+  "lat" => +48.8567,
+  "long" => +2.3508,
+  "country_id" => country2.id
+  })
+city5 = City.new({
+  "name" => "Nice",
+  "lat" => +43.7034,
+  "long" => +7.2663,
+  "country_id" => country2.id
+  })
+city6 = City.new({
+  "name" => "Berlin",
+  "lat" => +52.516667,
+  "long" => +13.388889,
+  "country_id" => country3.id
+  })
+city7 = City.new({
+  "name" => "Munich",
+  "lat" => +48.133333,
+  "long" => +11.566667,
+  "country_id" => country3.id
+  })
+city8 = City.new({
+  "name" => "Nice",
+  "lat" => +50.116667,
+  "long" => +8.683333,
+  "country_id" => country3.id
+  })
+
+city1.save()
+city2.save()
+city3.save()
+city4.save()
+city5.save()
+city6.save()
+city7.save()
+city8.save()
+
+  destination1 = Destination.new({
+    "isvisited" => "t",
+    "city_id" => city1.id
+    })
+
+  destination2 = Destination.new({
+    "isvisited" => "f",
+    "city_id" => city2.id
+    })
+
+  destination1.save()
+  destination2.save()
---
+++ db/travel_bucket_list.sql
@@ -0,0 +1,33 @@
+/*
+Terminal commands:
+createdb travel_bucket_list
+psql -d travel_bucket_list -f db/travel_bucket_list.sql
+*/
+
+DROP TABLE destinations;
+DROP TABLE cities;
+DROP TABLE countries;
+
+CREATE TABLE countries
+(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  continent VARCHAR(20)
+);
+
+CREATE TABLE cities
+(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  lat FLOAT,
+  long FLOAT,
+  country_id INT8 REFERENCES countries(id)
+);
+
+CREATE TABLE destinations
+(
+  id SERIAL8 PRIMARY KEY,
+  isvisited BOOLEAN,
+  city_id INT8 REFERENCES cities(id)
+
+);
