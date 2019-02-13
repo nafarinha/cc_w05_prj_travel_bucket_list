3d52375b133f11cf7d466741e70445d935ea264e
Nuno Farinha <nadfarinha@gmail.com>
2019-02-13 12:33:32 +0000

    Create sinatra app file

---
+++ app.rb
@@ -0,0 +1,10 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+# require_relative('controllers/country_controller')
+require_relative('controllers/city_controller')
+require_relative('controllers/destination_controller')
+
+get '/' do
+  erb( :index )
+end
