8d0a7887b78e3506d4aa452d53c1e57647286af9
Nuno Farinha <nadfarinha@gmail.com>
2019-02-13 22:09:31 +0000

    Create new action for destinations view

--- controllers/destination_controller.rb
+++ controllers/destination_controller.rb
@@ -7,3 +7,16 @@ get '/destinations' do
   @destinations = Destination.all()
   erb ( :"destinations/index" )
 end
+
+get '/destinations/new' do
+  @destinations = Destination.all()
+  @cities = City.all()
+  @countries = Country.all()
+  erb(  :"destinations/new"  )
+end
+
+post '/destinations' do
+  destination = Destination.new(params)
+  destination.save()
+  redirect to("/destinations")
+end
