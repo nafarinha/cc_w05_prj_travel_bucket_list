675e617c49138184d63db185939d89c42a25aa4b
Nuno Farinha <nadfarinha@gmail.com>
2019-02-13 17:47:14 +0000

    Create new action for city view

--- controllers/city_controller.rb
+++ controllers/city_controller.rb
@@ -9,6 +9,18 @@ get '/cities' do
   erb ( :"cities/index" )
 end
 
+get '/cities/new' do
+  @cities = City.all()
+  @countries = Country.all()
+  erb(  :"cities/new"  )
+end
+
+post '/cities' do
+  city = City.new(params)
+  city.save()
+  redirect to("/cities")
+end
+
 get '/cities/:id' do
   @city = City.find(params['id'].to_i)
   erb( :"cities/show" )
